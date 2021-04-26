be2e0f6fb9d9cf913b7bf35573ec1217ae425d4b
李宏涛 <frankhtli@tencent.com>
2021-04-26 23:33:48 +0800

    add deploy scripts

---
+++ install.sh
@@ -0,0 +1,149 @@
+#!/bin/bash
+trap 'exit' SIGTERM SIGINT
+
+#公共函数
+function LOG_ERROR()
+{
+	if (( $# < 1 ))
+	then
+		echo -e "\033[33m usesage: LOG_ERROR msg \033[0m";
+	fi
+
+	local msg=$(date +%Y-%m-%d" "%H:%M:%S);
+
+    msg="${msg} $@";
+
+	echo -e "\033[31m $msg \033[0m";
+}
+
+function LOG_WARNING()
+{
+	if (( $# < 1 ))
+	then
+		echo -e "\033[33m usesage: LOG_WARNING msg \033[0m";
+	fi
+
+	local msg=$(date +%Y-%m-%d" "%H:%M:%S);
+
+    msg="${msg} $@";
+
+	echo -e "\033[33m $msg \033[0m";
+}
+
+function LOG_DEBUG()
+{
+	if (( $# < 1 ))
+	then
+		LOG_WARNING "Usage: LOG_DEBUG logmsg";
+	fi
+
+	local msg=$(date +%Y-%m-%d" "%H:%M:%S);
+
+    msg="${msg} $@";
+
+ 	echo -e "\033[40;37m $msg \033[0m";
+}
+
+function LOG_INFO()
+{
+	if (( $# < 1 ))
+	then
+		LOG_WARNING "Usage: LOG_INFO logmsg";
+	fi
+
+	local msg=$(date +%Y-%m-%d" "%H:%M:%S);
+
+	for p in $@
+	do
+		msg=${msg}" "${p};
+	done
+
+	echo -e "\033[32m $msg \033[0m"
+}
+
+TARS_WEB_HOST=$1
+TARS_WEB_TOKEN=$2
+ADMIN_SERVER_IP=$3
+NODE_SERVER_IP=$4
+LOG_INFO "Tars Web Host:${TARS_WEB_HOST}"
+LOG_INFO "Tars Web Token:${TARS_WEB_TOKEN}"
+LOG_INFO "Tars Admin IP:${ADMIN_SERVER_IP}"
+LOG_INFO "Tars Node IP:${NODE_SERVER_IP}"
+
+## install TarsBenchmark
+git clone https://github.com/TarsCloud/TarsBenchmark.git TarsBenchmark
+cd TarsBenchmark || exit 1
+./TarsBenchmark/install.sh "${TARS_WEB_HOST}" "${TARS_WEB_TOKEN}" "${ADMIN_SERVER_IP}" "${NODE_SERVER_IP}"
+
+#exec sql
+function exec_mysql_sql()
+{
+    LOG_DEBUG "mysql -h${MYSQL_HOST} -u${MYSQL_USER} -p${MYSQL_PASS} -P${MYSQL_PORT} --default-character-set=utf8 -D$1 < $2"
+    mysql -h${MYSQL_HOST} -u${MYSQL_USER} -p${MYSQL_PASS} -P${MYSQL_PORT} --default-character-set=utf8 -D$1 < $2
+
+    ret=$?
+    LOG_DEBUG "mysql return:${ret}"
+
+    return $ret
+}
+MYSQL_PASS=$1
+exec_mysql_sql db_tars sql/services.sql
+
+## start to build TestUnits
+git clone https://github.com/TarsTestToolKit/TestUnits.git
+
+## TestUnits.GolangTars
+LOG_INFO "upload_and_publish TestUnits.GolangTars"
+curl -i --request POST "http://127.0.0.1:3000/api/upload_and_publish" \
+  --form "suse=@./patches/GolangTars.tgz" \
+  --form "application=TestUnits" \
+  --form "module_name=GolangTars" \
+  --form "comment=developer-auto-upload"
+
+## TestUnits.PhpTars
+LOG_INFO "upload_and_publish TestUnits.PhpTars"
+curl -i --request POST "http://127.0.0.1:3000/api/upload_and_publish" \
+  --form "suse=@./patches/PhpTars.tgz" \
+  --form "application=TestUnits" \
+  --form "module_name=PhpTars" \
+  --form "comment=developer-auto-upload"
+
+## TestUnits.JavaTars
+LOG_INFO "upload_and_publish TestUnits.JavaTars"
+curl -i --request POST "http://127.0.0.1:3000/api/upload_and_publish" \
+  --form "suse=@./patches/JavaTars.tgz" \
+  --form "application=TestUnits" \
+  --form "module_name=JavaTars" \
+  --form "comment=developer-auto-upload"
+
+## TestUnits.CppTars
+LOG_INFO "upload_and_publish TestUnits.CppTars"
+curl -i --request POST "http://127.0.0.1:3000/api/upload_and_publish" \
+  --form "suse=@./patches/CppTars.tgz" \
+  --form "application=TestUnits" \
+  --form "module_name=CppTars" \
+  --form "comment=developer-auto-upload"
+
+## TestUnits.NodejsTars
+LOG_INFO "upload_and_publish TestUnits.NodejsTars"
+curl -i --request POST "http://127.0.0.1:3000/api/upload_and_publish" \
+  --form "suse=@./patches/NodejsTars.tgz" \
+  --form "application=TestUnits" \
+  --form "module_name=NodejsTars" \
+  --form "comment=developer-auto-upload"
+
+## TarsTestToolKit.ResFetcher
+LOG_INFO "upload_and_publish TarsTestToolKit.ResFetcher"
+curl -i --request POST "http://127.0.0.1:3000/api/upload_and_publish" \
+  --form "suse=@./patches/ResFetcher.tgz" \
+  --form "application=TarsTestToolKit" \
+  --form "module_name=ResFetcher" \
+  --form "comment=developer-auto-upload"
+
+## TarsTestToolKit.BackendApi
+LOG_INFO "upload_and_publish TarsTestToolKit.BackendApi"
+curl -i --request POST "http://127.0.0.1:3000/api/upload_and_publish" \
+  --form "suse=@./patches/BackendApi.tgz" \
+  --form "application=TarsTestToolKit" \
+  --form "module_name=BackendApi" \
+  --form "comment=developer-auto-upload"
---
+++ sql/services.sql
@@ -0,0 +1,111 @@
+# noinspection SqlNoDataSourceInspectionForFile
+
+delete
+from `t_adapter_conf`
+where `application` = 'TestUnits'
+   or `application` = 'TarsTestToolKit';
+delete
+from `t_server_conf`
+where `application` = 'TestUnits'
+   or `application` = 'TarsTestToolKit';
+
+### TestUnits.CppTars
+replace into `t_adapter_conf` (`application`, `server_name`, `node_name`, `adapter_name`, `registry_timestamp`,
+                               `thread_num`, `endpoint`, `max_connections`, `allow_ip`, `servant`, `queuecap`,
+                               `queuetimeout`, `posttime`, `lastuser`, `protocol`, `handlegroup`)
+VALUES ('TestUnits', 'CppTars', 'localip.tars.com', 'TestUnits.CppTars.testObjAdapter', now(), 5,
+        'tcp -h localip.tars.com -t 60000 -p 22001', 2000, '', 'TestUnits.CppTars.testObj', 10000, 60000, now(), 'admin',
+        'tars', '');
+replace into `t_server_conf` (`application`, `server_name`, `node_group`, `node_name`, `registry_timestamp`,
+                              `base_path`, `exe_path`, `template_name`, `bak_flag`, `setting_state`, `present_state`,
+                              `process_id`, `patch_version`, `patch_time`, `patch_user`, `tars_version`, `posttime`,
+                              `lastuser`, `server_type`, `profile`)
+VALUES ('TestUnits', 'CppTars', '', 'localip.tars.com', now(), '', '', 'tars.cpp.default', 0, 'active', 'active', 0,
+        '2.1.0', now(), '', '2.1.0', now(), 'admin', 'tars_cpp', '');
+
+### TestUnits.JavaTars
+replace into `t_adapter_conf` (`application`, `server_name`, `node_name`, `adapter_name`, `registry_timestamp`,
+                               `thread_num`, `endpoint`, `max_connections`, `allow_ip`, `servant`, `queuecap`,
+                               `queuetimeout`, `posttime`, `lastuser`, `protocol`, `handlegroup`)
+VALUES ('TestUnits', 'JavaTars', 'localip.tars.com', 'TestUnits.JavaTars.testObjAdapter', now(), 5,
+        'tcp -h localip.tars.com -t 60000 -p 22002', 2000, '', 'TestUnits.JavaTars.testObj', 10000, 60000, now(), 'admin',
+        'tars', '');
+replace into `t_server_conf` (`application`, `server_name`, `node_group`, `node_name`, `registry_timestamp`,
+                              `base_path`, `exe_path`, `template_name`, `bak_flag`, `setting_state`, `present_state`,
+                              `process_id`, `patch_version`, `patch_time`, `patch_user`, `tars_version`, `posttime`,
+                              `lastuser`, `server_type`, `profile`)
+VALUES ('TestUnits', 'JavaTars', '', 'localip.tars.com', now(), '', '', 'tars.springboot', 0, 'active', 'active', 0,
+        '2.1.0', now(), '', '2.1.0', now(), 'admin', 'tars_java', '');
+
+
+### TestUnits.NodejsTars
+replace into `t_adapter_conf` (`application`, `server_name`, `node_name`, `adapter_name`, `registry_timestamp`,
+                               `thread_num`, `endpoint`, `max_connections`, `allow_ip`, `servant`, `queuecap`,
+                               `queuetimeout`, `posttime`, `lastuser`, `protocol`, `handlegroup`)
+VALUES ('TestUnits', 'NodejsTars', 'localip.tars.com', 'TestUnits.NodejsTars.testObjAdapter', now(), 5,
+        'tcp -h localip.tars.com -t 60000 -p 22003', 2000, '', 'TestUnits.NodejsTars.testObj', 10000, 60000, now(), 'admin',
+        'tars', '');
+replace into `t_server_conf` (`application`, `server_name`, `node_group`, `node_name`, `registry_timestamp`,
+                              `base_path`, `exe_path`, `template_name`, `bak_flag`, `setting_state`, `present_state`,
+                              `process_id`, `patch_version`, `patch_time`, `patch_user`, `tars_version`, `posttime`,
+                              `lastuser`, `server_type`, `profile`)
+VALUES ('TestUnits', 'NodejsTars', '', 'localip.tars.com', now(), '', '', 'tars.NodejsTars.default', 0, 'active', 'active', 0,
+        '2.1.0', now(), '', '2.1.0', now(), 'admin', 'tars_nodejs', '');
+
+
+### TestUnits.GolangTars
+replace into `t_adapter_conf` (`application`, `server_name`, `node_name`, `adapter_name`, `registry_timestamp`,
+                               `thread_num`, `endpoint`, `max_connections`, `allow_ip`, `servant`, `queuecap`,
+                               `queuetimeout`, `posttime`, `lastuser`, `protocol`, `handlegroup`)
+VALUES ('TestUnits', 'GolangTars', 'localip.tars.com', 'TestUnits.GolangTars.testObjAdapter', now(), 5,
+        'tcp -h localip.tars.com -t 60000 -p 22004', 2000, '', 'TestUnits.GolangTars.testObj', 10000, 60000, now(), 'admin',
+        'tars', '');
+replace into `t_server_conf` (`application`, `server_name`, `node_group`, `node_name`, `registry_timestamp`,
+                              `base_path`, `exe_path`, `template_name`, `bak_flag`, `setting_state`, `present_state`,
+                              `process_id`, `patch_version`, `patch_time`, `patch_user`, `tars_version`, `posttime`,
+                              `lastuser`, `server_type`, `profile`)
+VALUES ('TestUnits', 'GolangTars', '', 'localip.tars.com', now(), '', '', 'tars.default', 0, 'active', 'active', 0, '2.1.0',
+        now(), '', '2.1.0', now(), 'admin', 'tars_go', '');
+
+
+### TestUnits.PhpTars
+replace into `t_adapter_conf` (`application`, `server_name`, `node_name`, `adapter_name`, `registry_timestamp`,
+                               `thread_num`, `endpoint`, `max_connections`, `allow_ip`, `servant`, `queuecap`,
+                               `queuetimeout`, `posttime`, `lastuser`, `protocol`, `handlegroup`)
+VALUES ('TestUnits', 'PhpTars', 'localip.tars.com', 'TestUnits.PhpTars.testObjAdapter', now(), 5,
+        'tcp -h localip.tars.com -t 60000 -p 22005', 2000, '', 'TestUnits.PhpTars.testObj', 10000, 60000, now(), 'admin',
+        'tars', '');
+replace into `t_server_conf` (`application`, `server_name`, `node_group`, `node_name`, `registry_timestamp`,
+                              `base_path`, `exe_path`, `template_name`, `bak_flag`, `setting_state`, `present_state`,
+                              `process_id`, `patch_version`, `patch_time`, `patch_user`, `tars_version`, `posttime`,
+                              `lastuser`, `server_type`, `profile`)
+VALUES ('TestUnits', 'PhpTars', '', 'localip.tars.com', now(), '', '', 'tars.tarsphp.default', 0, 'active', 'active', 0,
+        '2.1.0', now(), '', '2.1.0', now(), 'admin', 'tars_php', '');
+
+###ResFetcher
+replace into `t_adapter_conf` (`application`, `server_name`, `node_name`, `adapter_name`, `registry_timestamp`,
+                               `thread_num`, `endpoint`, `max_connections`, `allow_ip`, `servant`, `queuecap`,
+                               `queuetimeout`, `posttime`, `lastuser`, `protocol`, `handlegroup`)
+VALUES ('TarsTestToolKit', 'ResFetcher', 'localip.tars.com', 'TarsTestToolKit.ResFetcher.fetcherObjAdapter', now(), 5,
+        'tcp -h localip.tars.com -t 60000 -p 22006', 2000, '', 'TarsTestToolKit.ResFetcher.fetcherObj', 10000, 60000,
+        now(), 'admin', 'tars', '');
+replace into `t_server_conf` (`application`, `server_name`, `node_group`, `node_name`, `registry_timestamp`,
+                              `base_path`, `exe_path`, `template_name`, `bak_flag`, `setting_state`, `present_state`,
+                              `process_id`, `patch_version`, `patch_time`, `patch_user`, `tars_version`, `posttime`,
+                              `lastuser`, `server_type`, `profile`)
+VALUES ('TarsTestToolKit', 'ResFetcher', '', 'localip.tars.com', now(), '', '', 'tars.default', 0, 'active', 'active',
+        0, '2.1.0', now(), '', '2.1.0', now(), 'admin', 'tars_go', '');
+
+###BackendApi
+replace into `t_adapter_conf` (`application`, `server_name`, `node_name`, `adapter_name`, `registry_timestamp`,
+                               `thread_num`, `endpoint`, `max_connections`, `allow_ip`, `servant`, `queuecap`,
+                               `queuetimeout`, `posttime`, `lastuser`, `protocol`, `handlegroup`)
+VALUES ('TarsTestToolKit', 'BackendApi', 'localip.tars.com', 'TarsTestToolKit.BackendApi.apiObjAdapter', now(), 5,
+        'tcp -h localip.tars.com -t 60000 -p 9001', 2000, '', 'TarsTestToolKit.BackendApi.apiObj', 10000, 60000, now(),
+        'admin', 'not_tars', '');
+replace into `t_server_conf` (`application`, `server_name`, `node_group`, `node_name`, `registry_timestamp`,
+                              `base_path`, `exe_path`, `template_name`, `bak_flag`, `setting_state`, `present_state`,
+                              `process_id`, `patch_version`, `patch_time`, `patch_user`, `tars_version`, `posttime`,
+                              `lastuser`, `server_type`, `profile`)
+VALUES ('TarsTestToolKit', 'BackendApi', '', 'localip.tars.com', now(), '', '', 'tars.default', 0, 'active', 'active',
+        0, '2.1.0', now(), '', '2.1.0', now(), 'admin', 'tars_go', '');
