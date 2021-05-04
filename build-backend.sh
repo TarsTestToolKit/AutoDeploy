3b9340286f82f097352ec90ec1643bd7cf4d22ad
李宏涛 <frankhtli@tencent.com>
2021-05-04 17:00:43 +0800

    add proxy on backend build

--- build-backend.sh
+++ build-backend.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 git clone https://github.com/TarsTestToolKit/ResFetcher.git
-docker run -v $(pwd)/ResFetcher:/root/src mcfranklee/tars-buildenv:latest sh -c "cd /root/src/ && rm -rf *.tgz && make && make tar"
+docker run -v $(pwd)/ResFetcher:/root/src mcfranklee/tars-buildenv:latest sh -c "cd /root/src/ && rm -rf *.tgz && go env -w GOPROXY='https://goproxy.cn,direct' && make && make tar"
 mv ResFetcher/ResFetcher.tgz patches/
 
 git clone https://github.com/TarsTestToolKit/BackendApi.git
-docker run -v $(pwd)/BackendApi:/root/src mcfranklee/tars-buildenv:latest sh -c "cd /root/src/ && rm -rf *.tgz && make && make tar"
+docker run -v $(pwd)/BackendApi:/root/src mcfranklee/tars-buildenv:latest sh -c "cd /root/src/ && rm -rf *.tgz && go env -w GOPROXY='https://goproxy.cn,direct' && make && make tar"
 mv BackendApi/BackendApi.tgz patches/
+
